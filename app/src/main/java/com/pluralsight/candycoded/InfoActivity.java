d51bb385ca17efb31047be4689ee4724f2103270
abooBala <abubakar.bala@whogohost.com>
2020-09-25 02:20:25 +0100

    Start Google Phone Activity - Modified createPhoneIntent()

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -38,7 +38,7 @@ public class InfoActivity extends AppCompatActivity {
 
     public void createPhoneIntent(View view){
         Uri uri = Uri.parse("tel:0123456789");
-        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL, uri);
         phoneIntent.setData(uri);
         startActivity(phoneIntent);
     }
